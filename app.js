c4b4a699aaf42dc37ca8653fceb8b9af78297f5d
Akanle Jesus <jesuswrites20043@gmail.com>
2023-09-07 22:55:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
-import express, { Request, Response } from 'express';
-import { utcToZonedTime, format } from 'date-fns-tz';
-
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; // Use port 3000 by default
 
-app.get('/api', (req: Request, res: Response) => {
+app.get('/api', (req, res) => {
   const { slack_name, track } = req.query;
 
   // Get the current day of the week
-  const timeZone = 'UTC';
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const currentDate = new Date();
-  const currentDay = format(utcToZonedTime(currentDate, timeZone), 'dddd', { timeZone });
+  const currentDay = daysOfWeek[currentDate.getUTCDay()];
 
   // Get the current UTC time with +/-2 minutes validation
   const currentUTC = new Date();
-  currentUTC.setMinutes(currentUTC.getUTCMinutes() - 2);
-
-  // Format the UTC time as "2023-09-07T21:21:16Z"
-  const formattedUTC = format(currentUTC, 'yyyy-MM-dd\'T\'HH:mm:ssXXX', { timeZone });
+  currentUTC.setMinutes(currentUTC.getUTCMinutes());
 
   // Construct the JSON response
   const response = {
     slack_name,
     current_day: currentDay,
-    utc_time: formattedUTC,
+    utc_time: currentUTC.toISOString(),
     track,
     github_file_url: 'https://github.com/username/repo/blob/main/file_name.ext',
     github_repo_url: 'https://github.com/username/repo',
